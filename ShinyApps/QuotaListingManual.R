f19c6cea7e2aba34fdfef26c3862796db0af28a5
gamaynard <galphonsemaynard@gmail.com>
2020-08-07 08:56:26 -0400

    fresh start

--- ShinyApps/QuotaListingManual.R
+++ ShinyApps/QuotaListingManual.R
@@ -1,36 +1,57 @@
+## ---------------------------
+##
+## Script name: 
+##
+## Purpose of script:
+##
+## Author: George A. Maynard
+##
+## Date Created: 2020-08-07
+##
+## Copyright (c) George Alphonse Maynard, 2020
+## Email: galphonsemaynard@gmail.com
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+## set working directory
+
+## ---------------------------
+
+## Set options
+options(scipen = 6, digits = 4) # eliminate scientific notation
+## ---------------------------
+
+## load up the packages we will need:  (uncomment as required)
 library(shiny)
+library(shinyjs)
+library(dplyr)
+library(DT)
+library(digest)
+## ---------------------------
 
-# Define the fields we want to save from the form
-fields <- c("name", "used_shiny", "r_num_years")
+## load up our functions into memory
 
-# Shiny app with 3 fields that the user can submit data for
+## ---------------------------
 shinyApp(
   ui = fluidPage(
-    DT::dataTableOutput("responses", width = 300), tags$hr(),
-    textInput("name", "Name", ""),
-    checkboxInput("used_shiny", "I've built a Shiny app in R before", FALSE),
-    sliderInput("r_num_years", "Number of years using R",
-                0, 25, 2, ticks = FALSE),
-    actionButton("submit", "Submit")
+    titlePanel("Mimicking a Google Form with a Shiny app"),
+    div(
+      id = "form",
+      
+      textInput("name", "Name", ""),
+      textInput("favourite_pkg", "Favourite R package"),
+      checkboxInput("used_shiny", "I've built a Shiny app in R before", FALSE),
+      sliderInput("r_num_years", "Number of years using R", 0, 25, 2, ticks = FALSE),
+      selectInput("os_type", "Operating system used most frequently",
+                  c("",  "Windows", "Mac", "Linux")),
+      actionButton("submit", "Submit", class = "btn-primary")
+    )
   ),
   server = function(input, output, session) {
-    
-    # Whenever a field is filled, aggregate all form data
-    formData <- reactive({
-      data <- sapply(fields, function(x) input[[x]])
-      data
-    })
-    
-    # When the Submit button is clicked, save the form data
-    observeEvent(input$submit, {
-      saveData(formData())
-    })
-    
-    # Show the previous responses
-    # (update with current response when Submit is clicked)
-    output$responses <- DT::renderDataTable({
-      input$submit
-      loadData()
-    })     
   }
 )
