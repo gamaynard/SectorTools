19c9666fd198cd5b37aa521cf07894df13e74d43
gamaynard <galphonsemaynard@gmail.com>
2020-08-07 10:56:12 -0400

    everything works, testing example set

--- ShinyApps/QuotaListingManual.R
+++ ShinyApps/QuotaListingManual.R
@@ -35,23 +35,92 @@ library(digest)
 ## ---------------------------
 
 ## load up our functions into memory
-
+## Create a star label for mandatory fields
+labelMandatory <- function(label) {
+  tagList(
+    label,
+    span("*", class = "mandatory_star")
+  )
+}
+humanTime <- function() format(Sys.time(), "%Y%m%d-%H%M%OS")
 ## ---------------------------
+fieldsMandatory <- c("name", "favourite_pkg")
+
+labelMandatory <- function(label) {
+  tagList(
+    label,
+    span("*", class = "mandatory_star")
+  )
+}
+humanTime <- function() format(Sys.time(), "%Y%m%d-%H%M%OS")
+appCSS <-
+  ".mandatory_star { color: red; }"
+fieldsAll <- c("name", "favourite_pkg", "used_shiny", "r_num_years", "os_type")
+responsesDir <- "C:/Users/George/Desktop/Autotask Workplace/Common/Mooncusser Sector, Inc/Database/responses/"
+epochTime <- function() {
+  as.integer(Sys.time())
+}
+
 shinyApp(
   ui = fluidPage(
+    shinyjs::useShinyjs(),
+    shinyjs::inlineCSS(appCSS),
     titlePanel("Mimicking a Google Form with a Shiny app"),
+    
     div(
       id = "form",
-      
-      textInput("name", "Name", ""),
-      textInput("favourite_pkg", "Favourite R package"),
+      textInput("name", labelMandatory("Name"), ""),
+      textInput("favourite_pkg", labelMandatory("Favourite R package")),
       checkboxInput("used_shiny", "I've built a Shiny app in R before", FALSE),
       sliderInput("r_num_years", "Number of years using R", 0, 25, 2, ticks = FALSE),
       selectInput("os_type", "Operating system used most frequently",
                   c("",  "Windows", "Mac", "Linux")),
       actionButton("submit", "Submit", class = "btn-primary")
-    )
+    ),
+    shinyjs::hidden(
+      div(
+        id = "thankyou_msg",
+        h3("Thanks, your response was submitted successfully!"),
+        actionLink("submit_another", "Submit another response")
+      )
+    )  
   ),
   server = function(input, output, session) {
+    observe({
+      mandatoryFilled <-
+        vapply(fieldsMandatory,
+               function(x) {
+                 !is.null(input[[x]]) && input[[x]] != ""
+               },
+               logical(1))
+      mandatoryFilled <- all(mandatoryFilled)
+      
+      shinyjs::toggleState(id = "submit", condition = mandatoryFilled)
+    })
+    formData <- reactive({
+      data <- sapply(fieldsAll, function(x) input[[x]])
+      data <- c(data, timestamp = epochTime())
+      data <- t(data)
+      data
+    })
+    saveData <- function(data) {
+      fileName <- sprintf("%s_%s.csv",
+                          humanTime(),
+                          digest::digest(data))
+      
+      write.csv(x = data, file = file.path(responsesDir, fileName),
+                row.names = FALSE, quote = TRUE)
+    }
+    # action to take when submit button is pressed
+    observeEvent(input$submit, {
+      saveData(formData())
+      shinyjs::reset("form")
+      shinyjs::hide("form")
+      shinyjs::show("thankyou_msg")
+    })
+    observeEvent(input$submit_another, {
+      shinyjs::show("form")
+      shinyjs::hide("thankyou_msg")
+    })    
   }
 )
